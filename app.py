d66f4c77d809e978ffc3ddab6a699fc09ab1dc8d
rochelle lewis <rochelle.a.lewis@gmail.com>
2021-11-23 15:56:44 -0700

    add get_locale(), GET req to Location Svcs api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 import os
 
 app = Flask(__name__)
@@ -10,6 +11,15 @@ API_KEY = os.environ.get("API_KEY")
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+def get_locale(location_type, query):
+    headers = {'Accept': 'application/json'}
+    url = f'https://api.weather.com/v3/location/search?query={query}&locationType={location_type}&language=en' \
+          f'-US&format=json&apiKey={API_KEY}'
+    r = requests.get(url, headers=headers)
+    data = r.json()
+    return data
 
 
 @app.route('/documentation')
